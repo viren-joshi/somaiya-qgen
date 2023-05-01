7a9e1f707e11a2f66029bdbcde2424bed25c578e
Viren Joshi <viren.joshi403@gmail.com>
2023-05-01 20:34:37 +0530

    added cron job for model API

---
+++ Workflows/ping_model.py
@@ -0,0 +1,4 @@
+from gradio_client import Client
+
+client = Client("https://devashishbhake-question-generation.hf.space/")
+result = client.predict("Ping Request. Pls dont shut down.", api_name="/predict")
