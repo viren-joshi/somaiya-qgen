1b3d1a34e5aaa4bdb0e95f5fe9f42736a98e0b75
Viren Joshi <viren.joshi403@gmail.com>
2023-05-02 08:51:07 +0530

    Update ping_model.py

--- .github/workflows/ping_model.py
+++ .github/workflows/ping_model.py
@@ -2,3 +2,4 @@ from gradio_client import Client
 
 client = Client("https://devashishbhake-question-generation.hf.space/")
 result = client.predict("Ping Request. Pls dont shut down.", api_name="/predict")
+print(result)
